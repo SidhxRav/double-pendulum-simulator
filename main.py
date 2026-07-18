7ca938e2f6278430a41a02040585985f46268f5b
Sidh Rav <sidh.raval786@gmail.com>
2026-07-18 11:10:11 +0800

    Just added some comments :)

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+#initialise graph
 fig, ax = plt.subplots(figsize=(6,6))
 ax.set_xlim(-3, 3)
 ax.set_ylim(-3, 3)
 ax.set_aspect('equal')
 line, = ax.plot([], [], 'o-', lw=2)
 
+#Set up inputs
 gravity = input("What planet?")
 gravties = {
     "Earth" : 9.8
@@ -19,6 +21,7 @@ r1 = int(input("Rope1 length in m?"))
 r2 = int(input("Rope2 length in m?"))
 theta1 = (int(input("Starting angle1 in degrees?")))*(np.pi/180)
 theta2 = (int(input("Starting angle2 in degrees?")))*(np.pi/180)
+#This just means it starts at rest (no initial velocity)
 thetadot1 = 0
 thetadot2 = 0
 dt = 0.001
@@ -26,11 +29,13 @@ dt = 0.001
 def update(frame):
     global theta1, theta2, thetadot1, thetadot2
 
+    #Calculate for the x and why values of the masses
     x1 = r1*np.sin(theta1)
     y1 = -r1*np.cos(theta1)
     x2 = r2* np.sin(theta2) + r1*np.sin(theta1)
     y2 = -r2*np.cos(theta2) - r1*np.cos(theta1)
 
+    #Set up the variables for solving theta double dots
     a = -(m1 +m2)*g*r1*np.sin(theta1) - m2*r1*r2*(thetadot2**2)*np.sin(theta1-theta2)
     b = (m1+m2)* (r1**2)
     c = m2*r1*r2*np.cos(theta1-theta2)
@@ -38,15 +43,19 @@ def update(frame):
     f = m2*(r2**2)
     h = m2*r1*r2*np.cos(theta1-theta2)
 
+    #Set up theta double dots
     thetaddot2 = (e-((a*h)/b))/(f-((c*h)/b))
 
     thetaddot1 = (a/b) - ((c/b)*thetaddot2)
     
+    #Use euler integration to find theta dots
     thetadot2 += thetaddot2*dt
     thetadot1 += thetaddot1*dt
 
+    #Use euler integration again to find theta dots
     theta2 += thetadot2*dt
     theta1 += thetadot1*dt
 
+    #return pivot, m1,m2 positions
     line.set_data([0, x1, x2], [0, y1, y2])
     return line,
