dd111b431ca0a5d71b12830c5a130f23e2c40d99
Sidh Rav <sidh.raval786@gmail.com>
2026-07-18 12:14:13 +0800

    i managed to speed up the animation by ensuring that while every frame was calculated, only eveyr n*30th frame would be stored which speeds up the animation by amount needed.
    although this error still seems wrong:
    
    Energy error: +0.0245% and -0.0072%
    Max energy: -27.6202
    Min energy: -27.6290
    Samples collected: 1869

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ r1 = int(input("Rope1 length in m?"))
 r2 = int(input("Rope2 length in m?"))
 theta1 = (int(input("Starting angle1 in degrees?")))*(np.pi/180)
 theta2 = (int(input("Starting angle2 in degrees?")))*(np.pi/180)
+#user chooses how fast animation plays
+steps_per_frame = (int(input("Playback speed?"))) * 30
 #This just means it starts at rest (no initial velocity)
 thetadot1 = 0
 thetadot2 = 0
@@ -54,26 +56,29 @@ def update(frame):
     x2 = r2* np.sin(theta2) + r1*np.sin(theta1)
     y2 = -r2*np.cos(theta2) - r1*np.cos(theta1)
 
-    #Set up the variables for solving theta double dots
-    a = -(m1 +m2)*g*r1*np.sin(theta1) - m2*r1*r2*(thetadot2**2)*np.sin(theta1-theta2)
-    b = (m1+m2)* (r1**2)
-    c = m2*r1*r2*np.cos(theta1-theta2)
-    e = -m2*g*r2*np.sin(theta2) + m2*r1*r2*(thetadot1**2)*np.sin(theta1-theta2)
-    f = m2*(r2**2)
-    h = m2*r1*r2*np.cos(theta1-theta2)
+    #Calculates every frame and then after this function is passed, only the nth frame will be played
+    #This makes sure the calculation has less error (at dt is still kept high) and the speed is faster
+    for i in range(steps_per_frame):
+        #Set up the variables for solving theta double dots
+        a = -(m1 +m2)*g*r1*np.sin(theta1) - m2*r1*r2*(thetadot2**2)*np.sin(theta1-theta2)
+        b = (m1+m2)* (r1**2)
+        c = m2*r1*r2*np.cos(theta1-theta2)
+        e = -m2*g*r2*np.sin(theta2) + m2*r1*r2*(thetadot1**2)*np.sin(theta1-theta2)
+        f = m2*(r2**2)
+        h = m2*r1*r2*np.cos(theta1-theta2)
 
-    #Set up theta double dots
-    thetaddot2 = (e-((a*h)/b))/(f-((c*h)/b))
+        #Set up theta double dots
+        thetaddot2 = (e-((a*h)/b))/(f-((c*h)/b))
 
-    thetaddot1 = (a/b) - ((c/b)*thetaddot2)
-    
-    #Use euler integration to find theta dots
-    thetadot2 += thetaddot2*dt
-    thetadot1 += thetaddot1*dt
+        thetaddot1 = (a/b) - ((c/b)*thetaddot2)
+        
+        #Use euler integration to find theta dots
+        thetadot2 += thetaddot2*dt
+        thetadot1 += thetaddot1*dt
 
-    #Use euler integration again to find theta dots
-    theta2 += thetadot2*dt
-    theta1 += thetadot1*dt
+        #Use euler integration again to find theta dots
+        theta2 += thetadot2*dt
+        theta1 += thetadot1*dt
     # Ensure that energies is constant
     total_energy = Calculate_energy(g,m1,m2,r1,r2,theta1,theta2,thetadot1,thetadot2 )
     print(total_energy)
