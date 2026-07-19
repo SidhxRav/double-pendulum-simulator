0339a65daa609d88a8e9a98d45c39702966e7f03
Sidh Rav <sidh.raval786@gmail.com>
2026-07-19 18:57:27 +0800

    initalising the website

--- Deploying phase/app.py
+++ Deploying phase/app.py
@@ -1,1 +1,13 @@
-hello
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
